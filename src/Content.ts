2e7da719b7422db2b1bc2846050804ca38c72115
nagy-krisztian <krisznagy2001@gmail.com>
2020-10-08 10:37:53 +0200

    2020.10.08 órai munka

--- src/Content.ts
+++ src/Content.ts
@@ -1,6 +1,6 @@
 ﻿import fs from "fs";
 import http from "http";
-import url from "url";
+import Megoldás from "./Megoldás";
 
 export default class Content {
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
@@ -22,8 +22,14 @@ export default class Content {
         res.write("<body><form><pre class='m-3'>");
 
         // Kezd a kódolást innen -->
-        
-        
+        // 1. feladat: adatok beolvasása, tárolása
+        const megold: Megoldás = new Megoldás("utasadat.txt");
+
+        // 2. feladat:
+        res.write(`2. feladat\nA buszra ${megold.felszállókSzáma} utas akar felszállni.`);
+
+        // 3. feladat
+        res.write(`3. feladat\nA buszra ${megold.érvénytelenFelszállás} utas nem szállhatott fel.`);
 
         // <---- Fejezd be a kódolást
 
--- src/Megoldás.ts
+++ src/Megoldás.ts
@@ -1,13 +1,31 @@
 import fs from "fs";
 import Felszállás from "./Felszállás";
+import FelSzállásBérlet from "./FelszállásBérlet";
+import FelszállásJegy from "./FelSzállásJegy";
 
 export default class Megoldás {
     private _utasAdatok: Felszállás[] = [];
+
+    public get felszállókSzáma(): number {
+        return this._utasAdatok.length;
+    }
+
+    public get érvénytelenFelszállás(): number {
+        return this._utasAdatok.filter(x => !x.érvényesFelszállás).length;
+    }
 
     constructor(forrás: string) {
         fs.readFileSync(forrás)
             .toString()
             .split("\n")
-            .forEach(i => {});
+            .forEach(i => {
+                const aktSor: string = i.trim();
+                const aktTipus: string = aktSor.split(" ")[3];
+                if (aktTipus == "JGY") {
+                    this._utasAdatok.push(new FelszállásJegy(aktSor));
+                } else if (["FEB", "TAB", "NYB", "NYP", "RVS", "GYK"].includes(aktTipus)) {
+                    this._utasAdatok.push(new FelSzállásBérlet(aktSor));
+                }
+            });
     }
 }
